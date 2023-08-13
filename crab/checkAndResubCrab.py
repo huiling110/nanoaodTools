686677fce07cd8fb31f6864b7d5b398981229e0d
huiling110 <huiling2016@outlook.com>
2023-08-13 14:23:02 +0200

    checkAndResubCrab.py can check crab status now

--- crab/checkAndResubCrab.py
+++ crab/checkAndResubCrab.py
@@ -1,8 +1,9 @@
 import os
-
+import subprocess
 
 def main():
-    crabDir = '/afs/cern.ch/work/h/hhua/nanoAOD/CMSSW_10_6_18/src/PhysicsTools/NanoAODTools/crab/crab_crabNanoPost/'
+    # crabDir = '/afs/cern.ch/work/h/hhua/nanoAOD/CMSSW_10_6_18/src/PhysicsTools/NanoAODTools/crab/crab_crabNanoPost/'
+    crabDir = '/afs/cern.ch/work/h/hhua/nanoAOD/CMSSW_10_6_18/src/PhysicsTools/NanoAODTools/crab/crab_crabNanoPost_2022PostEE/'
     
     for ipro in os.listdir(crabDir):
         print('i job: ', ipro)
@@ -11,6 +12,10 @@ def main():
    
 def checkStatus(jobDir):
     command = 'crab status -d ' + jobDir 
+    print('command: ', command)
+    run = subprocess.run(command, shell=True)
+    print(run.stdout)
+    
     
     
 if __name__=='__main__':
